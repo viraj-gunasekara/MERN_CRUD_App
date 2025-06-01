9a1ccc4a4f987b5507986dc9d01937854d1d1ebf
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-01 15:46:28 +0530

    configure mongodb connection with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
-//index/main/app.js
-//entry point for the api
+//index/main/app.js //entry point for the api
 
 //import express & creat express app
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 //create a route with get method
-app.get("/", (req,res) => {
-    res.send("Server is ready");
+app.get("/products", (req,res) => {
+    
 });
+
+//view: access to db uri
+// console.log(process.env.MONGO_URI);
 
 //listen a port
 app.listen(5000, () => {
+    //call the db connection
+    connectDB();
+
     console.log("Server started at http://localhost:5000 ");
 });
