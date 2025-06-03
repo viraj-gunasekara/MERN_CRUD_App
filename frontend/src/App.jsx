edb518e6692a6f9c9e6d193bfef74c2c1b21eca7
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-03 14:12:40 +0530

    setup routes with Navbar on top of all

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Box } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
 
+import HomePage from "./pages/HomePage.jsx"
+import CreatePage from "./pages/CreatePage.jsx"
+import Navbar from "./components/Navbar.jsx"
+
 function App() {
   return (
     <Box minH={"100vh"}>
-      {/* Navbar */}
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>} />
