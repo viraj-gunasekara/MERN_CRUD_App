671418c6bc41d32100bfc317352651141d3b459f
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-06-03 15:05:49 +0530

    navbar set

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Button, Container, Flex, HStack, Link, Text } from "@chakra-ui/react";
+import { FaRegSquarePlus } from "react-icons/fa6";
+import { MdOutlineLightMode } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize={{
+            base: "22",
+            sm: "28",
+          }}
+          fontWeight="bold"
+          textTransform={"uppercase"}
+          textAlign={"center"}
+        >
+          <Link to={"/"}>Shopping Store</Link>
+        </Text>
 
-export default Navbar
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <FaRegSquarePlus fontSize={20} />
+            </Button>
+          </Link>
+          <Button>
+            <MdOutlineLightMode fontSize={20} />
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
